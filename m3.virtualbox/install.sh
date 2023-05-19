752c71db72ca344cb31106a78585bb74fb0bf0a2
Manuel Wyssen <me@m3ns1.com>
2023-05-19 08:22:09 +0200

    wip

--- m3.virtualbox/install.sh
+++ m3.virtualbox/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O virtualbox.deb - https://download.virtualbox.org/virtualbox/6.1.36/virtualbox-6.1_6.1.36-152435~Ubuntu~jammy_amd64.deb
+wget -O virtualbox.deb - https://download.virtualbox.org/virtualbox/6.1.38/virtualbox-6.1_6.1.38-153438~Ubuntu~jammy_amd64.deb
 
 sudo apt -y install --fix-broken ./virtualbox.deb
 
